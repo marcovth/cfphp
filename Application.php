8f8c749fe801ddd2606f05d21565db672b40d57d
marcovth <marco@vanhethoog.com>
2020-05-04 11:52:53 -0400

    Update Application.php

--- Application.php
+++ Application.php
@@ -29,8 +29,8 @@ if(UCASE($cffileExt)==="CFML" or UCASE($cffileExt)==="CFM" or UCASE($cffileExt)=
 		echo "	.ace_editor { \n";
 		echo "		border: 1px solid lightgray; \n";
 		echo "		margin: auto; \n";
-		echo "		height: 600px; \n";
-		echo "		width: 50%; \n";
+		echo "		height: 300px; \n";
+		echo "		width: 100%; \n";
 		echo "	} \n";
 		echo "	.scrollmargin { \n";
 		echo "		height: 80px; \n";
@@ -40,14 +40,10 @@ if(UCASE($cffileExt)==="CFML" or UCASE($cffileExt)==="CFM" or UCASE($cffileExt)=
 		
 		echo "<form>\n";
 		echo "<table width=\"100%\">\n";
-		echo "	<tr><td align=center colspan=2><input type=button value=\"Save CFML code\"> <input type=button value=\"CFM->PHP\"> <input type=button value=\"Save PHP code\"></td></tr>\n";
-		echo "	<tr>\n";
-		echo "		<td valign=\"top\" id=\"editor1\" width=\"50%\"><textarea rows=40 name=\"CFMcode\">".$cp_CFcode."</textarea></td>\n";//$cp_CFcode.
-		echo "		<td valign=\"top\" id=\"editor2\" width=\"50%\"><textarea rows=40 name=\"PHPcode\">".$cp_PHPcode."</textarea></td>\n";//$cp_PHPcode.
-		echo "	</tr>\n";
-		echo "	<tr>\n";
-		echo "		<td valign=\"top\" id=\"PHPpage\" width=\"100%\" colspan=2><iframe width=\"100%\" height=\"100%\"  src=\"./$cffileName.php\"></iframe></td>\n";
-		echo "	</tr>\n";
+		echo "	<tr><td align=center><input type=button value=\"Translate: (overwrites)CFM => (overwrites)PHP_temp\"> <input type=button value=\"Save (overwrite) PHP_temp to PHP_final page\"></td></tr>\n";
+		echo "	<tr><td valign=\"top\" id=\"editor1\" width=\"50%\"><textarea rows=20 name=\"CFMcode\">".$cp_CFcode."</textarea></td></tr>\n";//$cp_CFcode.
+		echo "	<tr><td valign=\"top\" id=\"editor2\" width=\"50%\"><textarea rows=20 name=\"PHPcode\">".$cp_PHPcode."</textarea></td></tr>\n";//$cp_PHPcode.</tr>\n";
+		echo "	<tr><td valign=\"top\" id=\"PHPpage\" width=\"100%\"><iframe width=\"100%\" height=\"600\"  src=\"./$cffileName.php\"></iframe></td></tr>\n";
 		echo "</table>\n";
 		echo "</form>\n";
 		
@@ -73,6 +69,18 @@ if(UCASE($cffileExt)==="CFML" or UCASE($cffileExt)==="CFM" or UCASE($cffileExt)=
 		echo "	editor1.renderer.on(\"afterRender\", function(e) {\n";
 		echo "		if (editor2.session.getScrollTop() != editor1.session.getScrollTop()) {\n";
 		echo "			editor2.session.setScrollTop(editor1.session.getScrollTop());\n";
+		echo "		 }\n";
+		echo "		if (editor2.session.getScrollLeft() != editor1.session.getScrollLeft()) {\n";
+		echo "			editor2.session.setScrollLeft(editor1.session.getScrollLeft());\n";
+		echo "		 }\n";
+		echo "	})\n";
+		
+		echo "	editor2.renderer.on(\"afterRender\", function(e2) {\n";
+		echo "		if (editor1.session.getScrollTop() != editor2.session.getScrollTop()) {\n";
+		echo "			editor1.session.setScrollTop(editor2.session.getScrollTop());\n";
+		echo "		 }\n";
+		echo "		if (editor1.session.getScrollLeft() != editor2.session.getScrollLeft()) {\n";
+		echo "			editor1.session.setScrollLeft(editor2.session.getScrollLeft());\n";
 		echo "		 }\n";
 		echo "	})\n";
 	
