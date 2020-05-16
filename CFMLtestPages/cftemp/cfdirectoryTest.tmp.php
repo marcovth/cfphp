eb4ac0ed04e5756c5572ea0ab6c263c3dfaaaebc
marcovth <marco@vanhethoog.com>
2020-05-16 08:35:51 -0400

    -

--- CFMLtestPages/cftemp/cfdirectoryTest.tmp.php
+++ CFMLtestPages/cftemp/cfdirectoryTest.tmp.php
@@ -3,6 +3,7 @@
 <?php  $nrow=0; while($row = $myList->fetchArray()) { $CurrentRow=$nrow+1;for($ncol=0; $ncol< $myList->numColumns(); $ncol++) { ${$myList->columnName($ncol)}=$row[$ncol]; } $nrow++; $myList_Recordcount=$nrow; $Recordcount=$nrow; //CFOUTPUT ?>
     <li><?php echo $name; ?> | <?php echo $size; ?> | <?php echo $type; ?> | </li>
 <?php }//cfoutput ?>
+<li><?php echo $myList_Recordcount; ?></li>
 
 <?php $SQL=""; ?><?php $SQL.="SELECT * FROM myList WHERE type='Dir'"; ?>
 
